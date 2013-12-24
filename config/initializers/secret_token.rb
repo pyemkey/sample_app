40fcc361a55151db7b23bbb895a9f149914fbd5d
Michal Kloda <py.emkey@gmail.com>
2013-12-24 13:42:50 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = 'a947458a2e0e6ba60928b812637d641fad12efd127aff379d4142bdbdc8b724fe4fb9a1b76b1b69da1626d8176b4e533d8515ed69668793dedd4aea041fb93c6'
