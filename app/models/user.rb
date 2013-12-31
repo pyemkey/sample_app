d2dda683d1eef92b8e41705e1f0002087929d315
Michal Kloda <py.emkey@gmail.com>
2013-12-31 08:23:49 +0100

    Fix some email regex bugs

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,8 +14,8 @@ class User < ActiveRecord::Base
   attr_accessible :name, :email, :password, :password_confirmation
   has_secure_password
 
-  before_save { |user| user.email = user.email.downcase }
-
+ #before_save { |user| user.email = user.email.downcase }
+  before_save {self.email.downcase!}
   validates :name, presence: true, length: { maximum: 50}
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[\w+\-.]+\.[a-z]+\z/i
   validates :email, presence: true, format: {with: VALID_EMAIL_REGEX},
