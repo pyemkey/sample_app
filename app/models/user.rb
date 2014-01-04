64ae6ed2bac42941bf9840a930cbe9d566b0063f
Michal Kloda <py.emkey@gmail.com>
2014-01-04 20:56:34 +0100

    Finish sign in

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,6 +16,7 @@ class User < ActiveRecord::Base
 
  #before_save { |user| user.email = user.email.downcase }
   before_save {self.email.downcase!}
+  before_save :create_remember_token
   validates :name, presence: true, length: { maximum: 50}
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[\w+\-.]+\.[a-z]+\z/i
   validates :email, presence: true, format: {with: VALID_EMAIL_REGEX},
@@ -23,6 +24,11 @@ class User < ActiveRecord::Base
 
   validates :password, length: { minimum: 6}
   validates :password_confirmation, presence: true 
+
+  private
+    def create_remember_token
+      self.remember_token = SecureRandom.urlsafe_base64
+    end
 end
 
 
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -39,28 +39,29 @@ describe "UserPages" do
       end
     end
 
-	describe "with valid information" do
+		describe "with valid information" do
 
-		before do
-			fill_in "Name",         with: "Example User"
-			fill_in "Email",        with: "user@example.com"
-			fill_in "Password",     with: "foobar"
-			fill_in "Confirmation", with: "foobar"
-		end
+			before do
+				fill_in "Name",         with: "Example User"
+				fill_in "Email",        with: "user@example.com"
+				fill_in "Password",     with: "foobar"
+				fill_in "Confirmation", with: "foobar"
+			end
 
-		it "should create a user" do
-			expect { click_button submit }.to change(User, :count)
-		end
+			it "should create a user" do
+				expect { click_button submit }.to change(User, :count)
+			end
 
-		describe "after saving a user" do
+			describe "after saving a user" do
 
-			before { click_button submit}   
+				before { click_button submit}   
 
-			let(:user) { User.find_by_email("user@example.com") }
+				let(:user) { User.find_by_email("user@example.com") }
 
-			it { should have_selector('title', text: user.name)}
-			it { should have_selector('div.alert.alert-success', text: "Welcome") }
-		end
-    end
+				it { should have_selector('title', text: user.name)}
+				it { should have_selector('div.alert.alert-success', text: "Welcome") }
+        it { should have_link('Sign out') }
+			end
+	  end
   end
 end
